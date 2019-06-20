c7e70f65bbca7a985bd8a27d8f83f6578034858b
ninabreznik <ninabreznik@gmail.com>
2019-06-20 22:42:54 +0100

    Fix bug with args

--- bundle.js
+++ bundle.js
@@ -18396,11 +18396,11 @@ const validator = require('solidity-validator')
 
 module.exports = displayByteInput
 
-function displayByteInput ({ theme: { classes: css }, cb }) {
-  return input = bel`<div class=${css.byteField}> <input class=${css.inputField} data-type="byte" oninput=${validate} placeholder='0x...'> </div>`
+function displayByteInput ({ theme: { classes: css }, type, cb }) {
+  return input = bel`<div class=${css.byteField}> <input class=${css.inputField} data-type=${type} oninput=${validate} placeholder='0x...'> </div>`
   function validate (e) {
     let value = e.target.value
-    cb(validator.getMessage('byte', value), value)
+    cb(validator.getMessage(type, value), value)
   }
 }
 
